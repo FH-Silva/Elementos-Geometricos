99f2b9ff1c675697881f830f39a66fc5ff3984da
Fabio Henrique <160084746+FH-Silva@users.noreply.github.com>
2024-04-16 14:00:33 +0000

    estilos formas

--- script/quadrado.js
+++ script/quadrado.js
@@ -3,5 +3,5 @@ botao.addEventListener("click", calculadora); //Espera o evento
 function calculadora (event){ //Função de quando o evento ocorrer
     event.preventDefault(); //Para de atualizar automaticamente
     let lados = document.getElementById("ladoQuadrado").value; //Pega o valor do forms
-    document.getElementById("saida").innerHTML = lados * lados; //Faz o calculo e exibe
+    document.getElementById("saida").innerHTML = "Resultado: " + lados * lados; //Faz o calculo e exibe
 };
