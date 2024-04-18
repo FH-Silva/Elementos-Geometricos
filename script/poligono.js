69bffed2baeadabd253254df018426bd0a06e4a1
Fabio Henrique <fabiosilva5210@gmail.com>
2024-04-18 01:38:20 -0300

    final

--- script/poligono.js
+++ script/poligono.js
@@ -2,32 +2,33 @@ var botao = document.getElementById("enviar");
 botao.addEventListener("click", function(event){
     var escolha = document.getElementById("escolha").value;
     var apotema = document.getElementById("apotema").value;
+    var perimetro = document.getElementById("perimetro").value;
     var result;
-    if(apotema !== ""){
+    if(apotema !== "" && perimetro !== ""){
     switch(escolha){
         case "pentagono":
-            result = parseFloat(apotema);
-            event.preventDefault();
+            result = 5 * parseFloat(perimetro) * parseFloat(apotema);
             break;
         case "hexagono":
-            alert("Ok");
+            result = ((3 * parseFloat(perimetro))**2 * Math.sqrt(3)) / 2;
             break;
         case "heptagono":
-            alert("Ok");
+            result = 7 * parseFloat(perimetro) * parseFloat(apotema) / 2;
             break;
         case "octagono":
-            alert("Ok");
+            result = 4 * parseFloat(perimetro) * parseFloat(apotema);
             break;
         case "eneagono":
-            alert("Ok");
+            result = 9 * parseFloat(perimetro) * parseFloat(apotema) / 2;
             break;
         case "decagono":
-            alert("Ok");
+            result = 5 * parseFloat(perimetro) * parseFloat(apotema);
             break;
         default:
             alert("Pane no sistema, alguém me desconfiguro :(");
     }
-    document.getElementById("saida").innerText = "Resultado do " + escolha + " é: " + result;
+    event.preventDefault();
+    document.getElementById("saida").innerText = "Resultado do " + escolha + " é: " + result.toFixed(2);
     }else{
         alert("Um dos formulários ou todos os formulários estão vazios, digite novamente");
     }
